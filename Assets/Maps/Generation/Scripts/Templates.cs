f40929c6503633e518cefa4efbbe2f6e385bacf0
axu1672 <axu1672@gmail.com>
2021-06-28 00:24:50 -0400

    Adding Wrapper Class for MapTemplates
    Added Center room template class. No Full implementation

--- Assets/Maps/Generation/Scripts/Templates.cs
+++ Assets/Maps/Generation/Scripts/Templates.cs
@@ -18,4 +18,57 @@ public interface IMapTemplate
     Vector2 GridPosition { get; set; }
 
     GameObject TemplateAsset { get; set; }
+    string type { get; }
+    void Load(GameObject clone);
+}
+
+
+public class CenterTemplate : IMapTemplate
+{
+
+    public string type => "Center";
+
+    // This is for a room with 4 open doors.
+    private Vector3 _rawPosition;
+    public Vector3 RawPosition { 
+        get
+        {
+            return _rawPosition;
+        }
+        set
+        {
+            _rawPosition = value;
+        }
+    }
+
+    Vector2 _gridPosition;
+    public Vector2 GridPosition 
+    {
+        get
+        {
+            return _gridPosition;
+        }
+        set
+        {
+            _gridPosition = value;
+        }
+    }
+
+    private GameObject _templateAsset;
+    public GameObject TemplateAsset 
+    {
+        get
+        {
+            return _templateAsset;
+        }
+        set
+        {
+            _templateAsset = value;
+        }
+    }
+
+    public void Load(GameObject clone)
+    {
+
+    }
 }
